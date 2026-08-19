a8da68618ee96ee7e6476035c9b1103488f61883
abhayd-dev <abhaydwivedi427@gmail.com>
2026-08-19 21:56:51 +0530

    Temporarily force APP_DEBUG to true to catch 500 error

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => true, // Temporarily forced to true
 
     /*
     |--------------------------------------------------------------------------
